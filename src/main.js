71f3a6f43830e8474694bb77c3da76b822814ff1
bocampoc95 <breyner.95.bo@gmail.com>
2024-10-02 22:14:31 -0300

    update main js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
-import './style.css';
-import './styles/index.css';
 import Acess from './components/ButtumSIA.js';
 import ASIDE from './components/AsideSale.js';
 import Header from './components/header.js';
 import Footer from './components/footer.js';
 
-
-
-// let acces = document.querySelector<HTMLDivElement>('#access_cp');
-// acces.innerHTML =  Acess;
-// let header =document.querySelector<HTMLDivElement>('#header_cp');
-// header.innerHTML = Header;
-// document.querySelector<HTMLDivElement>('#aside_cp').innerHTML = ASIDE;
-// document.querySelector<HTMLDivElement>('#footer_cp').innerHTML = Footer;
 let acces = document.getElementById("access_cp");
 let aside = document.getElementById("aside_cp");
 let header = document.getElementById("header_cp");
