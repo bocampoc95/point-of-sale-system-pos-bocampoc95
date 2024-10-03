1c14ef6d87fe7d935982c979c68812eed632c3d2
bocampoc95 <161968193+bocampoc95@users.noreply.github.com>
2024-10-02 23:09:44 -0300

    Update ButtumSIA.js

--- src/components/ButtumSIA.js
+++ src/components/ButtumSIA.js
@@ -4,7 +4,7 @@
 const Acess =`<!-- Button Accessibility -->
 <div class="fixed bottom-3/4 right-0 w-[40px] h-[40px] flex items-center justify-center ">
   <div class=" w-full h-full rounded-full shadow-md  ">
-    <img class="w-8 h-8 " src="./public/avatar.svg" alt="Accessibility Button" />
+    <img class="w-8 h-8 " src="./public/Acess.svg" alt="Accessibility Button" />
   </div>
 </div>
 <!-- Button Accessibility -->`
